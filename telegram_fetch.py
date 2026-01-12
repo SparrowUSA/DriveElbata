59160e20966e7dc4f14b29c0e7bee5caabfac166
Sparrow <lol327970@gmail.com>
2026-01-12 19:16:52 +0530

    Update telegram_fetch.py

--- telegram_fetch.py
+++ telegram_fetch.py
@@ -1,8 +1,50 @@
-from telegram import InputMediaDocument, InputMediaVideo, InputMediaPhoto
+from telegram import Message
+from telegram.ext import ContextTypes
 
-async def get_messages_from_channel(client, chat_id, limit):
-    messages = []
-    async for msg in client.get_chat_history(chat_id, limit=limit):
-        if msg.video or msg.document or msg.photo:
-            messages.append(msg)
-    return list(reversed(messages))
+
+async def fetch_single_file(message: Message):
+    """
+    Extracts a single Telegram file object + filename
+    Works for: photo, video, document, audio
+    """
+
+    if message.document:
+        file = await message.document.get_file()
+        filename = message.document.file_name or "document.bin"
+
+    elif message.video:
+        file = await message.video.get_file()
+        filename = message.video.file_name or "video.mp4"
+
+    elif message.photo:
+        file = await message.photo[-1].get_file()
+        filename = "photo.jpg"
+
+    elif message.audio:
+        file = await message.audio.get_file()
+        filename = message.audio.file_name or "audio.mp3"
+
+    else:
+        return None
+
+    return file, filename
+
+
+
+async def fetch_from_channel(context: ContextTypes.DEFAULT_TYPE, channel_id: int, limit: int = 100):
+    """
+    Fetch last N media messages from a channel.
+    Works for private channels if bot is a member.
+    """
+
+    result = []
+
+    async for msg in context.bot.get_chat_history(chat_id=channel_id, limit=limit):
+
+        data = await fetch_single_file(msg)
+
+        if data:
+            result.append(data)
+
+    return result
+
